f0cd162c04dfed9f1850a0cc9e37deeec398b6ba
marlon <marlonsuyuc@gmail.com>
2024-01-12 19:28:39 -0600

    add zusatn to proyect

---
+++ src/store/ui/ui-store.ts
@@ -0,0 +1,17 @@
+import { create } from 'zustand'
+
+interface State {
+  isOpenSideMenu: boolean
+  openSideMenu: () => void
+  closeSideMenu: () => void
+}
+
+export const useUIStore = create<State>()((set) => ({
+  isOpenSideMenu: false,
+  openSideMenu: () => {
+    set({ isOpenSideMenu: true })
+  },
+  closeSideMenu: () => {
+    set({ isOpenSideMenu: false })
+  }
+}))
